3aa4c04704578aee07b3b6022de56aea70135eae
Satya Santika <55965465+satyasantika@users.noreply.github.com>
2023-11-06 09:28:26 +0700

    edit guide quota information for dosen dashboard

--- resources/views/dashboard/dosen.blade.php
+++ resources/views/dashboard/dosen.blade.php
@@ -1,14 +1,127 @@
 
 <h3>Selamat datang, {{ auth()->user()->name }}</h3>
+<div class="row">
+    <div class="col-6">
+        <div class="card">
+            <div class="card-header">Info hasil pemilihan pembimbing di tahun 2023</div>
+            <div class="card-body">
+                untuk melihat hasil pemilihan pembimbing tahap 1,2,3,dst... silakan klik tombol berikut:<br>
+                <a href="{{ route('respons.result') }}" class="btn btn-sm btn-primary">Pemilihan Pembimbing</a>
+            </div>
+        </div>
+    </div>
+    <div class="col-6">
+        <div class="card">
+            <div class="card-header">Terima/Tolak Usulan Pembimbing tahun 2023</div>
+            <div class="card-body">
+                untuk merespon pemilihan pembimbing di tahap 2 silakan klik tommbol berikut:<br>
+                <a href="{{ route('respons.index') }}" class="btn btn-sm btn-primary">proses tahap 2</a>
+            </div>
+        </div>
+    </div>
+</div>
+
 <hr>
-untuk melihat hasil pemilihan pembimbing tahap 1 silakan klik tombol berikut:<br>
-<a href="{{ route('respons.result') }}" class="btn btn-sm btn-primary">hasil tahap 1</a>
-<hr>
+<div class="h5">
+    Informasi kuota Pembimbing
+</div>
+@php
+        $guide = App\Models\GuideAllocation::where('user_id',auth()->user()->id)->where('active',1)->first()
+        @endphp
 
-untuk merespon pemilihan pembimbing di tahap 2 silakan klik tommbol berikut:<br>
-<a href="{{ route('respons.index') }}" class="btn btn-sm btn-primary">proses tahap 2</a>
-
-
-
+<div class="card">
+    <div class="card-header bg-light">Pembimbing 1</div>
+    <div class="card-body">
+        <div class="row">
+            <div class="col">
+                <div class="h1">
+                    {{ $guide->guide1_quota }}
+                </div>Kuota
+            </div>
+            <div class="col">
+                <div class="h1">
+                    {{ App\Models\SelectionGuide::where([
+                        'user_id'=>auth()->user()->id,
+                        'guide_order'=>1,
+                        ])->count() }}
+                </div>Usulan
+            </div>
+            <div class="col">
+                <div class="h1">
+                    {{ App\Models\SelectionGuide::where([
+                        'user_id'=>auth()->user()->id,
+                        'guide_order'=>1,
+                        'approved'=>1,
+                        ])->count() }}
+                </div>Acc
+            </div>
+            <div class="col">
+                <div class="h1">
+                    {{ App\Models\SelectionGuide::where([
+                        'user_id'=>auth()->user()->id,
+                        'guide_order'=>1,
+                        'approved'=>0,
+                        ])->count() }}
+                </div>Ditolak
+            </div>
+            <div class="col">
+                <div class="h1">
+                    {{ App\Models\SelectionGuide::where([
+                        'user_id'=>auth()->user()->id,
+                        'guide_order'=>1,
+                        'approved'=>NULL,
+                        ])->count() }}
+                </div>Menunggu
+            </div>
+        </div>
+    </div>
+</div>
+<div class="card mt-3">
+    <div class="card-header bg-light">Pembimbing 2</div>
+    <div class="card-body">
+        <div class="row">
+            <div class="col">
+                <div class="h1">
+                    {{ $guide->guide2_quota }}
+                </div>Kuota
+            </div>
+            <div class="col">
+                <div class="h1">
+                    {{ App\Models\SelectionGuide::where([
+                        'user_id'=>auth()->user()->id,
+                        'guide_order'=>2,
+                        ])->count() }}
+                </div>Usulan
+            </div>
+            <div class="col">
+                <div class="h1">
+                    {{ App\Models\SelectionGuide::where([
+                        'user_id'=>auth()->user()->id,
+                        'guide_order'=>2,
+                        'approved'=>1,
+                        ])->count() }}
+                </div>Acc
+            </div>
+            <div class="col">
+                <div class="h1">
+                    {{ App\Models\SelectionGuide::where([
+                        'user_id'=>auth()->user()->id,
+                        'guide_order'=>2,
+                        'approved'=>0,
+                        ])->count() }}
+                </div>Ditolak
+            </div>
+            <div class="col">
+                <div class="h1">
+                    {{ App\Models\SelectionGuide::where([
+                        'user_id'=>auth()->user()->id,
+                        'guide_order'=>2,
+                        'approved'=>NULL,
+                        ])->count() }}
+                </div>Menunggu
+            </div>
+        </div>
+    </div>
+</div>
 
 {{-- @includeWhen(auth()->user()->can('respon selection guide'), 'selection.guide-respon') --}}
