cbb01640b7367c41bc59ff013b8d82b4db911f97
Satya Santika <55965465+satyasantika@users.noreply.github.com>
2024-09-14 22:55:51 +0700

    edit format WA untuk pengingat belum menilai

--- resources/views/examination/admin/scoringyet.blade.php
+++ resources/views/examination/admin/scoringyet.blade.php
@@ -28,8 +28,8 @@
                                     <td>
                                         <a target="_blank" href="{{'https://api.whatsapp.com/send/?phone=62'
                                             .$exam_score->lecture->phone.'&text=Yth.%20Penguji%20'
-                                            .$exam_score->name.',%0A%0AMohon%20segera%20memberikan%20penilaian%20'
-                                            .$exam_score->registration->examtype->name.'%20pada%20'
+                                            .$exam_score->mahasiswa.',%0A%0AMohon%20segera%20memberikan%20penilaian%20'
+                                            .$exam_score->ujian.'%20pada%20'
                                             .$exam_score->registration->exam_date->isoFormat('dddd, D MMMM Y').'%20agar%20mahasiswa%20tersebut%20dapat%20segera%20mencetak%20lembar%20revisinya%0A%0Asilakan%20akses:%0A%0Ahttp://supportfkip.unsil.ac.id/dbsmatematika/examination/scoring/'
                                             .$exam_score->id.'/edit%0A%0A(jika%20eror%20saat%20buka%20link%20di%20handphone,%20pastikan%20awalannya%20http://%20bukan%20https://)'}}"
                                             class="badge rounded-pill bg-success btn btn-sm">wa</a>
@@ -41,7 +41,7 @@
                                         {{ $exam_score->mahasiswa }}
                                     </td>
                                     <td>{{ $exam_score->ujian }}</td>
-                                    <td>{{ $exam_score->exam_date }} {{ $exam_score->registration->exam_time }}</td>
+                                    <td>{{ $exam_score->exam_date }} {{ \Carbon\Carbon::create($exam_score->exam_time)->isoFormat('HH:mm') }}</td>
                                 </tr>
                                 @empty
                                 <tr>
